10693a13f962a577b982fccd6533b867da54e705
Tim Watson <tim.watson@youversion.com>
2025-07-21 11:52:25 -0500

    Updated the color theme to be correct on zudoku

--- devdocs/zudoku.config.tsx
+++ devdocs/zudoku.config.tsx
@@ -9,6 +9,50 @@ const config: ZudokuConfig = {
     },
     title: "YouVersion Bible API Documentation",
   },
+  theme: {
+    light: {
+      background: "oklch(100% 0 none)",
+      foreground: "oklch(13.7% 0.036 259)",
+      card: "oklch(100% 0 none)",
+      cardForeground: "oklch(13.7% 0.036 259)",
+      popover: "oklch(100% 0 none)",
+      popoverForeground: "oklch(13.7% 0.036 259)",
+      primary: "oklch(20.8% 0.04 266)",
+      primaryForeground: "oklch(98.4% 0.003 248)",
+      secondary: "oklch(96.8% 0.007 248)",
+      secondaryForeground: "oklch(20.8% 0.04 266)",
+      muted: "oklch(96.8% 0.007 248)",
+      mutedForeground: "oklch(55.5% 0.041 257)",
+      accent: "oklch(96.8% 0.007 248)",
+      accentForeground: "oklch(20.8% 0.04 266)",
+      destructive: "oklch(63.7% 0.208 25.3)",
+      destructiveForeground: "oklch(98.4% 0.003 248)",
+      border: "oklch(92.9% 0.013 256)",
+      input: "oklch(92.9% 0.013 256)",
+      ring: "oklch(13.7% 0.036 259)"
+    },
+    dark: {
+      background: "oklch(13.7% 0.036 259)",
+      foreground: "oklch(98.4% 0.003 248)",
+      card: "oklch(13.7% 0.036 259)",
+      cardForeground: "oklch(98.4% 0.003 248)",
+      popover: "oklch(13.7% 0.036 259)",
+      popoverForeground: "oklch(98.4% 0.003 248)",
+      primary: "oklch(98.4% 0.003 248)",
+      primaryForeground: "oklch(20.8% 0.04 266)",
+      secondary: "oklch(28% 0.037 260)",
+      secondaryForeground: "oklch(98.4% 0.003 248)",
+      muted: "oklch(28% 0.037 260)",
+      mutedForeground: "oklch(71.1% 0.035 257)",
+      accent: "oklch(28% 0.037 260)",
+      accentForeground: "oklch(98.4% 0.003 248)",
+      destructive: "oklch(39.6% 0.133 25.7)",
+      destructiveForeground: "oklch(98.4% 0.003 248)",
+      border: "oklch(28% 0.037 260)",
+      input: "oklch(28% 0.037 260)",
+      ring: "212.7 26.8% 83.9"
+    }
+  },
   navigation: [
     {
       type: "category",
@@ -87,5 +131,6 @@ const config: ZudokuConfig = {
     },
   ],
 };
+
 
 export default config;
