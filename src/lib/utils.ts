130ab377022d83a9d3d22e6913695bf958dc2c83
Brian Herbert <brian.herbert@youversion.com>
2025-08-05 09:52:18 -0500

    For /admin/ endpoints, use app id with correct permissions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,10 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { YVP_CONFIG } from './constants';
 
+// App ID constants
+const EXAMPLES_APP_ID = 'r4JBXiG5GAIbxQOBdhAw8k2yikjeE17l4nZ14FLEPIZLmGtN';
+const ADMIN_APP_ID = 'dkV1PqA2YwNdtzGGYlZWAxAk72mJDUWmVd6QeIRqr9WlLjX2';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -14,10 +18,14 @@ export async function yvpFetch(endpoint: string, options: RequestInit = {}): Pro
   // Get LAT token from localStorage
   const lat = localStorage.getItem('yvp_lat');
   
+  // Determine which app ID to use based on endpoint
+  const isAdminEndpoint = endpoint.startsWith('/admin/');
+  const appId = isAdminEndpoint ? ADMIN_APP_ID : EXAMPLES_APP_ID;
+  
   // Prepare headers
   const headers: HeadersInit = {
     'Accept': 'application/json',
-    'x-app-id': 'r4JBXiG5GAIbxQOBdhAw8k2yikjeE17l4nZ14FLEPIZLmGtN',
+    'x-app-id': appId,
     'x-yv-iid': 'install_id_example',
     ...options.headers,
   };
